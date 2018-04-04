26125e2081c867a7151ffa82e05440e17bd4bb90
ZZcc <2923400952@qq.com>
2018-04-04 23:40:42 +0800

    修改modify_date字段DEFAULT错误

--- database/create_database.sql
+++ database/create_database.sql
@@ -8,6 +8,6 @@ CREATE TABLE IF NOT EXISTS `s_paocao` (
   `card_no` bigint(20) NOT NULL COMMENT '一卡通号',
   `count_paocao` int(11) DEFAULT NULL COMMENT '跑操次数',
   `create_datetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
-  `modify_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `modify_date` timestamp NOT NULL DEFAULT '2000-01-01 00:00:00',
   PRIMARY KEY (`card_no`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
